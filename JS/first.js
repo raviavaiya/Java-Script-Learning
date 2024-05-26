62dc6a3be84d957b62278fe0aefcf9fabcbaa363
raviavaiya3 <21bmiit007@gmail.com>
2024-05-27 00:00:05 +0530

    First Lecture

---
+++ JS/first.js
@@ -0,0 +1,58 @@
+//console.log("Ravi avaiya");
+//console.log("My First Java Script Code");
+
+
+// Variables - COntainer that contains data
+
+name = "Ravi Avaiya";
+Name = "Mitul Avaiya"; // Case Sensitive
+age = 22;
+price = 95.2;
+x = null;
+y = undefined;
+isfollow = true;
+//console.log(Name);
+
+
+
+// Variable Declaration (var)
+//  var fullName="Ravi";
+// var fullName="Mitul";
+// console.log(fullName);
+
+
+//  Variable Declaration (let)
+// let fullName="Ravi";
+//     fullName="Mitul";
+// console.log(fullName);
+
+// Variable Declaration (const)
+// const fullName = "Ravi";
+// console.log(fullName);
+
+
+{
+    let fullName = "Ravi";
+    console.log(fullName);
+}
+{
+    let fullName = "Ravi";
+    console.log(fullName);
+}
+
+
+// Data Types of JS
+// Number, String, Boolean, Undefined, Null, BigInt, Symbol
+
+//Object
+
+const student = {
+    name: "Ravi Avaiya",
+    age : 22,
+    price : 95.2,
+};
+
+student["age"]=student["age"]+1;
+console.log(student["name"]);
+console.log(student.age);
+console.log(student.price);
